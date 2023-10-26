d601c878f7e61dfdfd991166c4816b96293b13ce
djumanov <diyorbekjumanov1120@gmail.com>
2023-10-26 19:34:35 +0500

    register

--- test_db.py
+++ test_db.py
@@ -3,7 +3,7 @@ from db import DB
 db = DB('database.json')
 
 def test_add_or_update_temp_user():
-    print(db.add_or_update_temp_user(chat_id='1234', group='a'))
+    print(db.add_or_update_temp_user(chat_id='1234', group="Vaiyev"))
 
 def test_add_user():
     db.add_user('1234')
